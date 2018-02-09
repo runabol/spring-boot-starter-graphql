471b8431fc3276b1396ebb6dfda83440e99b275b
Cohen, Arik <arik.cohen@smithsonianchannel.com>
2018-02-08 22:53:05 -0500

    wip

---
+++ src/main/java/com/creactiviti/spring/boot/starter/graphql/GraphQLAutoConfiguration.java
@@ -0,0 +1,15 @@
+package com.creactiviti.spring.boot.starter.graphql;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Arik Cohen
+ * @since Feb 07, 2018
+ */
+@Configuration
+@ComponentScan(basePackages="com.creactiviti.spring.boot.starter.graphql")
+public class GraphQLAutoConfiguration  {
+  
+  
+}
